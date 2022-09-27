28976a6cca48ace5a96ad94f90e9f51270de7ae4
sudeshnasc9 <sudeshnasc9@gmail.com>
2022-09-27 13:09:18 +0530

    Changes w.r.t. Passenger Transport Data in 12.27

--- src/functionalTest/java/io/github/vantiv/sdk/TestDepositTransactionReversal.java
+++ src/functionalTest/java/io/github/vantiv/sdk/TestDepositTransactionReversal.java
@@ -2,15 +2,12 @@ package io.github.vantiv.sdk;
 
 import static org.junit.Assert.assertEquals;
 
+import io.github.vantiv.sdk.generate.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import io.github.vantiv.sdk.generate.CustomBilling;
-import io.github.vantiv.sdk.generate.EnhancedData;
-import io.github.vantiv.sdk.generate.LodgingInfo;
-import io.github.vantiv.sdk.generate.ProcessingInstructions;
-import io.github.vantiv.sdk.generate.DepositTransactionReversal;
-import io.github.vantiv.sdk.generate.DepositTransactionReversalResponse;
+import java.math.BigInteger;
+import java.util.Calendar;
 
 public class TestDepositTransactionReversal {
 
@@ -54,5 +51,66 @@ public class TestDepositTransactionReversal {
         assertEquals("000", response.getResponse());
         assertEquals("id123", response.getId());
         assertEquals(987654L, response.getRecyclingResponse().getCreditCnpTxnId().longValue());
+    }
+    @Test
+    public  void simpleDepositTransactionReversalWithPassengerTransportData() throws Exception{
+        Authorization authorization = new Authorization();
+        authorization.setReportGroup("русский中文");
+        authorization.setOrderId("12344");
+        authorization.setAmount(106L);
+        authorization.setOrderSource(OrderSourceType.ECOMMERCE);
+        authorization.setId("id");
+        CardType card = new CardType();
+        card.setType(MethodOfPaymentTypeEnum.VI);
+        card.setNumber("4100000000000000");
+        card.setExpDate("1210");
+        authorization.setCard(card);
+        authorization.setPassengerTransportData(passengerTransportData());
+        AuthorizationResponse response = cnp.authorize(authorization);
+        assertEquals("русский中文",response.getReportGroup());
+        assertEquals("sandbox", response.getLocation());
+    }
+
+    private PassengerTransportData passengerTransportData(){
+        PassengerTransportData passengerTransportData = new PassengerTransportData();
+        passengerTransportData.setPassengerName("PassengerName");
+        passengerTransportData.setTicketNumber("9876543210");
+        passengerTransportData.setIssuingCarrier("str4");
+        passengerTransportData.setCarrierName("CarrierName");
+        passengerTransportData.setRestrictedTicketIndicator("RestrictedIndicator");
+        passengerTransportData.setNumberOfAdults(8);
+        passengerTransportData.setNumberOfChildren(1);
+        passengerTransportData.setCustomerCode("CustomerCode");
+        passengerTransportData.setArrivalDate(Calendar.getInstance());
+        passengerTransportData.setIssueDate(Calendar.getInstance());
+        passengerTransportData.setTravelAgencyCode("TravCode");
+        passengerTransportData.setTravelAgencyName("TravelAgencyName");
+        passengerTransportData.setComputerizedReservationSystem(ComputerizedReservationSystemEnum.DATS);
+        passengerTransportData.setCreditReasonIndicator(CreditReasonIndicatorEnum.C);
+        passengerTransportData.setTicketChangeIndicator(TicketChangeIndicatorEnum.C);
+        passengerTransportData.setTicketIssuerAddress("IssuerAddress");
+        passengerTransportData.setExchangeAmount(new Long(110));
+        passengerTransportData.setExchangeFeeAmount(new Long(112));
+        passengerTransportData.setExchangeTicketNumber("ExchangeNumber");
+        passengerTransportData.getTripLegDatas().add(addTripLegData());
+        return  passengerTransportData;
+    }
+
+    private TripLegData addTripLegData(){
+        TripLegData tripLegData = new TripLegData();
+        tripLegData.setTripLegNumber(new BigInteger("4"));
+        tripLegData.setDepartureCode("DeptC");
+        tripLegData.setCarrierCode("CC");
+        tripLegData.setServiceClass(ServiceClassEnum.BUSINESS);
+        tripLegData.setStopOverCode("N");
+        tripLegData.setDestinationCode("DestC");
+        tripLegData.setFareBasisCode("FareBasisCode");
+        tripLegData.setDepartureDate(Calendar.getInstance());
+        tripLegData.setOriginCity("OCity");
+        tripLegData.setTravelNumber("TravN");
+        tripLegData.setDepartureTime("DepartureTime");
+        tripLegData.setArrivalTime("ArrivalTime");
+        tripLegData.setRemarks("Remarks");
+        return  tripLegData;
     }
 }
